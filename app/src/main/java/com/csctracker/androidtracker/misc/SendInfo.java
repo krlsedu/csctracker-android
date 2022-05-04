8d11346e58dbfd6567c7b74dd7daa6150a442434
Carlos Eduardo Duarte Schwalm <krlsedu@gmail.com>
2022-05-04 20:01:13 -0300

    Notifications with security

--- app/src/main/java/com/csctracker/androidtracker/misc/SendInfo.java
+++ app/src/main/java/com/csctracker/androidtracker/misc/SendInfo.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import androidx.preference.PreferenceManager;
 import com.csctracker.androidtracker.service.monitor.core.SqlLitle;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.BufferedWriter;
@@ -21,47 +22,22 @@ public class SendInfo {
         sqlLitle = new SqlLitle(context);
     }
 
-    public void postData(String json) {
-        Thread thread = new Thread(() -> {
-            HttpURLConnection connection = null;
-            try {
-                Message message = new Message(json);
-                URL url = new URL("https://notify.csctracker.com/message");
-                connection = (HttpURLConnection) url.openConnection();
-                connection.setDoOutput(true);
-                connection.setRequestMethod("POST");
-                connection.setRequestProperty("Authorization", "Bearer " + preferences.getString(Const.PREF_TOKEN, ""));
-                connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8"); // here you are setting the `Content-Type` for the data you are sending which is `application/json`
-                connection.connect();
-
-                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream(), StandardCharsets.UTF_8));
-                bw.write(new ObjectMapper().writeValueAsString(message)); // I dunno how to write this string..
-                bw.flush();
-                bw.close();
-
-                int response = connection.getResponseCode();
-                if (response != 201) {
-
-                }
-            } catch (Exception e) {
-                try {
-                    sqlLitle.salva(json, "info");
-                } catch (Exception ex) {
-                    System.out.println(ex.getMessage());
-                }
-            }
-        });
-        thread.start();
-    }
-
     public void send(String json, String endpoint) {
 
-        String uri;
-        if ("message".equals(endpoint)) {
-            postData(json);
-        } else {
-            uri = preferences.getString(Const.PREF_URL, "https://backend.csctracker.com/") + endpoint;
-
+        Thread thread = new Thread(() -> {
+            String jsonSend = json;
+            String uri;
+            if ("message".equals(endpoint)) {
+                Message message = new Message(json);
+                try {
+                    jsonSend = new ObjectMapper().writeValueAsString(message);
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
+                uri = "https://notify.csctracker.com/message";
+            } else {
+                uri = preferences.getString(Const.PREF_URL, "https://backend.csctracker.com/") + endpoint;
+            }
             HttpURLConnection connection = null;
             try {
                 URL url = new URL(uri);
@@ -73,14 +49,14 @@ public class SendInfo {
                 connection.connect();
 
                 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream(), StandardCharsets.UTF_8));
-                bw.write(json);
+                bw.write(jsonSend);
                 bw.flush();
                 bw.close();
 
                 int response = connection.getResponseCode();
                 if (response != 201) {
                     try {
-                        sqlLitle.salva(json, endpoint);
+                        sqlLitle.salva(jsonSend, endpoint);
                     } catch (Exception e) {
                         //
                     }
@@ -92,7 +68,8 @@ public class SendInfo {
                     connection.disconnect();
                 }
             }
-        }
+        });
+        thread.start();
     }
 }
 
